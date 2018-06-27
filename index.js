69fefdf7710282556d6e03d0c9f36b117e4bb2ca
Pavel Zharov <trother555@gmail.com>
2018-06-27 18:08:20 +0300

    fix require

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-let bot = require('service/bot')
+let bot = require('./service/bot')
 let mongoose = require('mongoose')
 
 let connectionString = process.env.CON_STRING||'mongodb://localhost/test';
