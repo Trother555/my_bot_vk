f2ca993185f7aa46e777357321280742634577d0
Pavel Zharov <trother555@gmail.com>
2018-06-28 14:33:55 +0300

    final check

--- controller/game.js
+++ controller/game.js
@@ -3,13 +3,10 @@ let text = require('../text/text.json');
 
 let onRaise = async (handler) => {
     player = await Player.getOrCreatePlayer(handler.pid);
-    console.log(player);
     let raised = Math.round((Math.random()*10000)%10000);
     player.bablo += raised;
-    player
-    .save()
-    .then(()=>handler.text(`${text.raised[0]} ${raised} ${text.raised[1]}  ${player.bablo}`))
-    .catch((err)=>console.log(text.error, err));
+    await player.save();
+    handler.text(`${text.raised[0]} ${raised} ${text.raised[1]}  ${player.bablo}`))
 };
 
 module.exports = {
